2bbead97f9669ebda2916949bb436d4aa81dc53b
Nic Wilson <nicwilson@gmail.com>
2021-02-18 10:51:57 -0600

    feat: update to use octokit

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const core = require('@actions/core');
+const Octokit = require('@octokit/core');
+const { createAppAuth } = require("@octokit/auth-app");
 const jwt = require('jsonwebtoken');
 
-try {
-  const privateKey = core.getInput('private_key');
-  const appId = core.getInput('app_id');
-  const token = getJwtForApp(appId, privateKey);
-  core.setOutput('token', token);
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+    try {
+      const privateKey = core.getInput('private_key');
+      const appId = core.getInput('app_id');
+      const installationId = core.getInput('installation_id');
+      
+      const appOctokit = new Octokit({
+        authStrategy: createAppAuth,
+        auth: {
+          appId,
+          privateKey,
+        },
+      });
+      
+      const { token } = await appOctokit.auth({
+        installationId,
+        type: 'installation',
+      });
+
+      core.setOutput('token', token);
+    } catch (error) {
+      core.setFailed(error.message);
+    }
 }
 
-/**
- * Signs a JWT for a GitHub App installation that can be used to perform actions on behalf of the app
- * @param {string} appId GitHub App ID of Installed App
- * @param {string} privateKey GitHub App Private Key
- */
-function getJwtForApp(appId, privateKey) {
-  const epochTime = Math.round(Date.now() / 1000); // Convert from epoch time in milliseconds to seconds
-  const payload = {
-    // issued at time
-    iat: epochTime,
-    // JWT expiration time (10 minute maximum)
-    exp: epochTime + 10 * 60,
-    // GitHub App ID
-    iss: appId,
-  };
-  return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-}
-
-module.exports = getJwtForApp;
+main();
